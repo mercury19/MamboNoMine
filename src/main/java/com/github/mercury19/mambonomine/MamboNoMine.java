6b2c7fcca0024277d12c65309972d97f724df1bb
mercury19 <voltaicquicksilver@yahoo.com>
2020-06-19 23:36:38 -0700

    Copper Base Done (Ore gen, ingot, block, nugget)

--- src/main/java/com/github/mercury19/mambonomine/MamboNoMine.java
+++ src/main/java/com/github/mercury19/mambonomine/MamboNoMine.java
@@ -4,6 +4,7 @@ import com.github.mercury19.mambonomine.proxy.CommonProxy;
 import com.github.mercury19.mambonomine.recipes.SmeltingRecipes;
 import com.github.mercury19.mambonomine.tabs.MamboNoMineTab;
 import com.github.mercury19.mambonomine.util.Reference;
+import com.github.mercury19.mambonomine.world.gen.WorldGenOre;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -12,7 +13,8 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLServerStartingEvent;;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;;
 
 @Mod(modid = Reference.MODID, version = Reference.VERSION, name = Reference.NAME)
 public class MamboNoMine 
@@ -29,6 +31,8 @@ public class MamboNoMine
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event)
 	{
+		
+		GameRegistry.registerWorldGenerator(new WorldGenOre(), 4);
 		
 	}
 	
